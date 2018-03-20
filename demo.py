b7a56a1227769cde2bf1ae1cf8fd0921e736573e
Zhefan Ye <zhefanye@gmail.com>
2018-03-20 11:35:11 -0400

    misc update

--- demo.py
+++ demo.py
@@ -61,7 +61,7 @@ def parse_args():
                       nargs=argparse.REMAINDER)
   parser.add_argument('--load_dir', dest='load_dir',
                       help='directory to load models',
-                      default="/srv/share/jyang375/models")
+                      default="data/models")
   parser.add_argument('--image_dir', dest='image_dir',
                       help='directory to load images for demo',
                       default="images")
